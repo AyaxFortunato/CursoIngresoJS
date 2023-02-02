bdad3db42cbc6604e2e7dc475fa3a2057774c67f
Ayax Fortunato <ayaxfortunato@hotmail.com>
2023-02-02 09:20:25 -0300

    a

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,19 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
+let importe;
+let porcentaje;
+let resultado;
+let descuento;
+
+descuento = 25;
+
+importe = document.getElementById("").value;
+importe = parseFloat(importe);
+
+porcentaje = importe * desceunto /100;
+resultado = importe + porcentaje;
+
+document.getElementById("").value = resultado;
 	alert("ok");
 }
