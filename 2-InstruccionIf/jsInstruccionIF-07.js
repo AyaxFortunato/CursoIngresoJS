39fc423d0d8205c9e442bd3eccb8a3715e075da3
Ayax Fortunato <ayaxfortunato@hotmail.com>
2023-02-08 09:37:39 -0300

    a

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -2,8 +2,20 @@
 //Al ingresar una edad menor a 18 años y un estado civil distinto a "Soltero", mostrar el siguiente mensaje: 'Es muy pequeño para NO ser soltero.'
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	 let edad;
+	 let estadoCivil;
+	 let mensaje;
+
+	 edad = document.getElementById("txtIdEdad").value;
+	 edad = parseInt(edad);
+	 estadoCivil = document.getElementById("estadoCivil").value;
+
+	 if(edad < 18 && estadoCivil != "Soltero");
+	{
+		mensaje = "Es muy pequeño para no ser soltero"
+		alert(mensaje);
+	}
+
 	
 
 
