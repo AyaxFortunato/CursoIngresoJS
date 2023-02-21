ea801cca596d83f7b9aafcd8b5f968c16e50b46b
Ayax Fortunato <ayaxfortunato@hotmail.com>
2023-02-21 15:39:25 -0300

    a

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -1,6 +1,63 @@
+//Ayax Fortunato
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	let estacionIngresada;
+	let destino;
+	let mensaje;
+	let porcentaje;
+	let precio;
+	let precioFinal;
+
+	precio = 15000
+
+	estacionIngresada = document.getElementById("txtIdEstacion").value;
+	destino = document.getElementById("txtIdDestino").value;
+
+	switch(estacionIngresada){
+		case "Invierno":
+			switch(destino){
+				case"Bariloche":
+				porcentaje = 20;
+				break;
+			case "Cataratas":
+			case "Cordoba":
+				porcentaje = -10;
+				break;
+			case "Mar del plata":
+				porcentaje = -20;
+				break;
+			}
+			break;
+		case "Verano":
+			switch(destino){
+				case "Bariloche":
+					porcentaje = -20;
+					break;
+				case "Cataratas":
+				case "Cordoba":
+					porcentaje = 10;
+					break;
+				case "Mar del plata":
+					porcentaje = 20;
+					break;
+			}
+			break;
+			case "Otoño":
+			case "Primavera":
+				switch(destino){
+					case "Bariloche":
+					case "Cataratas":
+					case "Mar del plata":
+						porcentaje = 10;
+						break;
+					case "Cordoba":
+						porcentaje = 0;
+						break
+				}
+				break;
+	}		
+	precioFinal = precio + (precio* porcentaje / 100);
+	mensaje = "El precio del viaje es de" + precioFinal;
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,38 @@
+//Ayax Fortunato
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	let destino;
+	let estacionAnio;
+	let mensaje;
 
+	destino = document.getElementById("txtIdDestino").Value;
+	estacionAnio = document.getElementById("txtIdEstacion").Value;
+	
+	switch(estacionAnio)
+	{
+		case "Invierno":
+			if(destino == "Bariloche"){
+				mensaje = "Se viaja";
+			}else{
+				mensaje = "No se viaja";
+			}
+			break;
+		case "Verano":
+			if(destino == "Mar del plata" || destino == "Catamarca"){
+			mensaje = "Se viaja"
+			}else{
+			mensaje = "No se viaja";
+			}
+			break;
+		case "Otoño":
+			mensaje = "Se viaja";
+			break;
+		case "Primavera":
+			if(destino == "Mar del plata" || destino == "Cataratas" || destino == "Cordoba"){
+				mensaje = "Se viaja";
+			}else{
+				mensaje = "No se viaja";
+			}	
+			break;	}
+alert(mensaje);
 }//FIN DE LA FUNCIÓN
